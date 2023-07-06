ae91699b88f8bda81651cef91472febb3dcd2872
metaphor <tutianchi@gmail.com>
2023-07-06 12:00:57 +0800

    Revert "Revert "20230705-Update""
    This reverts commit 3c462c01896c6fa1c84e32525ec98cc2b9799bb2.

--- SSGEdit/SSGEdit/Gridcrtl/GridCtrl.cpp
+++ SSGEdit/SSGEdit/Gridcrtl/GridCtrl.cpp
@@ -618,7 +618,7 @@ void CGridCtrl::OnPaint()
 
     if (m_bDoubleBuffer)    // Use a memory DC to remove flicker
     {
-        CMemDC MemDC(&dc);
+        GCMemDC MemDC(&dc);
         OnDraw(&MemDC);
     }
     else                    // Draw raw - this helps in debugging vis problems.
